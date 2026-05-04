eae4b25ff11c242f0c471672dd45bd0467d4f670
Dasha2002 <kosolapova.dasha2018@yandex.ru>
2026-05-04 17:54:35 +0500

    Автоподбор страница

---
+++ includes/autofit-hero.php
@@ -0,0 +1,28 @@
+<div class="autofit-hero">
+    <div class="autofit-hero__bg">
+        <div class="autofit-hero__bg-img" style="background-image: url('assets/_1.jpg.webp');"></div>
+        <div class="autofit-hero__overlay"></div>
+    </div>
+
+    <div class="autofit-hero__container">
+        <div class="autofit-hero__content">
+            <h1 class="autofit-hero__title">Автоподбор по России</h1>
+
+            <p class="autofit-hero__price">От 16 000 ₽*</p>
+
+            <p class="autofit-hero__note">*Окончательная стоимость услуги<br>зависит от вашего бюджета и задач</p>
+
+            <div class="autofit-link">
+                <a href="#zakazat_uslugu" class="autofit-hero__btn btn-popap-two">Заказать услугу</a>
+
+                <a href="#details" class="autofit-hero__link">
+                    Что входит в услугу?
+                    <svg class="autofit-hero__arrow" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10 4V16M10 16L5 11M10 16L15 11" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                </a>
+            </div>
+
+        </div>
+    </div>
+</div>
---
+++ includes/autofit-inf-two.php
@@ -0,0 +1,36 @@
+<section class="stats-section" id="stats">
+    <div class="container-stats">
+        <div class="stats-header">
+            <h2 class="stats-title">
+                БОЛЕЕ <span class="stats-title__highlight">3 000 АВТОВЛАДЕЛЬЦЕВ</span> ВЫБРАЛИ НАС
+            </h2>
+            <p class="stats-subtitle">Для автоподбора и диагностики в Перми и по всей стране</p>
+        </div>
+        
+        <div class="stats-grid">
+            <div class="stats-item">
+                <div class="stats-item__value">94% СЛУЧАЕВ</div>
+                <div class="stats-item__line"></div>
+                <p class="stats-item__text">Когда услуги бесплатны<br>за счет торга с продавцом!</p>
+            </div>
+            
+            <div class="stats-item">
+                <div class="stats-item__value">10 - 21 ДНЕЙ</div>
+                <div class="stats-item__line"></div>
+                <p class="stats-item__text">Среднее время<br>подбора автомобиля</p>
+            </div>
+            
+            <div class="stats-item">
+                <div class="stats-item__value">97 ПАРАМЕТРОВ</div>
+                <div class="stats-item__line"></div>
+                <p class="stats-item__text">Мы проверяем<br>у каждого автомобиля</p>
+            </div>
+            
+            <div class="stats-item">
+                <div class="stats-item__value">238 000 ₽</div>
+                <div class="stats-item__line"></div>
+                <p class="stats-item__text">Самая большая сторгованная<br>сумма при покупке авто</p>
+            </div>
+        </div>
+    </div>
+</section>
